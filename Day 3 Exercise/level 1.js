2544a1bb4ba8e619d27d72de93c32e34517a254d
Abdussomad Pa-aranti <abdussomadabdulqodir@gmail.com>
2022-06-28 15:13:27 +0100

    Asabeneh

--- Day 3 Exercise/level 1.js
+++ Day 3 Exercise/level 1.js
@@ -1,3 +1,4 @@
+//1. Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
 let firstName = "Baaqi"
 let lastName = "Abdulqodir"
 let country = "Nigeria"
@@ -12,31 +13,47 @@ console.log(typeof city)
 console.log(typeof age)
 console.log(typeof isMarried)
 console.log(typeof year)
+
+//2. Check if type of '10' is equal to 10
 console.log(typeof "10" == 10)
+//3. Check if parseInt('9.8') is equal to 10
 console.log(parseInt('9.8') == 10)
-let a = 7;
-let c = 9;
-let x = 7;
-let z = 11;
-let sex = "male";
-let space = " ";
-console.log(a)
-console.log(sex)
-console.log(a = x)
-console.log(space)
-console.log(c < a)
-console.log(a === c)
-4 > 3 //true, because 4 is greater than 3
-4 >= 3 //true, because 4 is greater than 3
-4 < 3 //false, because 4 is not greater than 3
-4 <= 3 //false, because 4 is less and not equal to 3
-4 == 4 //true, because 4 is equal to 4
-4 === 4//true, because 4 is equivalent to 4
-4 != 4//false, because 4 is equal to 4
-4 !== 4//false
-4 != '4'//false
-4 == '4'//true
-4 === '4'//false
+//4. Boolean value is either true or false.
+//i. Write three JavaScript statement which provide truthy value.
+let a = 1000 === parseFloat("1000")
+let iAmMarried = true
+let c = "I love My Wife."
+
+//ii. Write three JavaScript statement which provide falsy value.
+let d = 1000 === "1000"
+let iAmNotMarried = false
+let e = ""
+//5. Figure out the result of the following comparison expression first without using console.log().After you decide the result confirm it using console.log()
+
+//i. 4 > 3
+//ii. 4 >= 3
+//iii. 4 < 3
+//iv. 4 <= 3
+//v. 4 == 4
+//vi. 4 === 4
+//vii. 4 != 4
+//viii. 4 !== 4
+//ix. 4 != '4'
+//x. 4 == '4'
+//xi. 4 === '4'
+//xii. Find the length of python and jargon and make a falsy comparison statement.
+
+//i. true, because 4 is greater than 3
+//ii. true, because 4 is greater and not equal to 3
+//iii. false, because 4 is not lesser than 3
+//iv. false, because 3 is less and not equal to 4
+//v. true, because 4 is equal to 4
+//vi. true, because 4 is equivalent to 4
+//vii. false, because 4 is equal to 4
+//viii. false
+//ix. false
+//x. true
+//xi. false
 console.log(4 > 3)
 console.log(4 >= 3)
 console.log(4 < 3)
@@ -48,21 +65,26 @@ console.log(4 !== 4)
 console.log(4 != '4')
 console.log(4 == '4')
 console.log(4 === '4')
+
 let p = "python"
 let j = "jargon"
 console.log(p.length)
 console.log(j.length)
 console.log(p.length !== j.length)
-4 > 3 && 10 < 12//true
-4 > 3 && 10 > 12//false
-4 > 3 || 10 < 12//true
-4 > 3 || 10 > 12//true
-!(4 > 3)//false
-!(4 < 3)//true
-!(false)//true
-!(4 > 3 && 10 < 12)//false
-!(4 > 3 && 10 > 12)//true
-!(4 === '4')//false
+
+//6. Figure out the result of the following expressions first without using console.log().After you decide the result confirm it by using console.log()
+
+//i. 4 > 3 && 10 < 12 true
+//ii. 4 > 3 && 10 > 12 false
+//iii. 4 > 3 || 10 < 12 true
+//iv. 4 > 3 || 10 > 12 true
+//v. !(4 > 3) false
+//vi. !(4 < 3) true
+//vii. !(false) true
+//viii. !(4 > 3 && 10 < 12) false
+//ix. !(4 > 3 && 10 > 12) true
+//x. !(4 === '4') true
+//xi. There is no 'on' in both dragon and python. false
 console.log(4 > 3 && 10 < 12)
 console.log(4 > 3 && 10 > 12)
 console.log(4 > 3 || 10 < 12)
@@ -73,15 +95,22 @@ console.log(!(false))
 console.log(!(4 > 3 && 10 < 12))
 console.log(!(4 > 3 && 10 > 12))
 console.log(!(4 === '4'))
-console.log(p.match(/on/gi))
-console.log(j.match(/on/gi))
+console.log(!(p.match(/on/gi)))
+console.log(!(j.match(/on/gi)))
+
+//7. Use the Date object to do the following activities
+//i. What is the year today ?
+//ii. What is the month today as a number ?
+//iii. What is the date today ?
+//iv. What is the day today as a number ?
+//v.  What is the hours now ?
+//vi. What is the minutes now ?
+//vii. Find out the numbers of seconds elapsed from January 1, 1970 to now.
+
 let now = new Date()
-console.log(now)
 console.log(now.getFullYear())
 console.log(now.getMonth())
 console.log(now.getDate())
 console.log(now.getDay())
 console.log(now.getHours())
 console.log(now.getMinutes())
-let d = Date.now()
-console.log(Date.now())
--- Day 3 Exercise/level 2.js
+++ Day 3 Exercise/level 2.js
@@ -1,19 +1,30 @@
+//1. Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle(area = 0.5 x b x h).
+
+// Enter base: 20
+// Enter height: 10
+// The area of the triangle is 100
+
 var triangleBase = prompt("Enter base of the Triangle")
 var triangleHeight = prompt("Enter height of the Triangle")
 const areaOfTriangle = 0.5 * triangleBase * triangleHeight
 alert("The area of the triangle is " + areaOfTriangle)
-console.log({ triangleBase })
-console.log({ triangleHeight })
-console.log({ areaOfTriangle })
+
+
+//2. Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle(perimeter = a + b + c)
+
+// Enter side a: 5
+// Enter side b: 4
+// Enter side c: 3
+// The perimeter of the triangle is 12
 
 var triangleSideA = prompt("Enter side a")
 var triangleSideB = prompt("Enter side b")
 var triangleSideC = prompt("Enter side c")
 const perimeterOfTriangle = (+triangleSideA + +triangleSideB + +triangleSideC)
-alert("The perimeter of the Triangle is" + perimeterOfTriangle)
-console.log({ triangleSideA })
-console.log({ triangleSideB })
-console.log({ triangleSideC })
+alert("The perimeter of the Triangle is " + perimeterOfTriangle)
+
+
+//3. Get length and width using prompt and calculate an area of rectangle(area = length x width and the perimeter of rectangle(perimeter = 2 x(length + width))
 
 var lengthOfRectangle = prompt("Enter your Rectangle length")
 var widthOfRectangle = prompt("Enter your Rectangle width")
@@ -21,58 +32,105 @@ const areaOfRectangle = lengthOfRectangle * widthOfRectangle
 const perimeterOfRectangle = 2 * (+lengthOfRectangle + +widthOfRectangle)
 alert("The area of Rectangle " + areaOfRectangle)
 alert("The perimeter of Rectangle " + perimeterOfRectangle)
-console.log({ lengthOfRectangle })
-console.log({ widthOfRectangle })
-console.log({ areaOfRectangle })
-console.log({ perimeterOfRectangle })
 
-var radius = prompt("Type in your Radius")
-const pi = 3.4
+//4. Get radius using prompt and calculate the area of a circle(area = pi x r x r) and circumference of a circle(c = 2 x pi x r) where pi = 3.14.
+
+let radius = prompt("Enter your radius")
+let pi = 3.14
 const areaOfCircle = pi * radius * radius
 const circumferenceOfCircle = 2 * pi * radius
-alert("The area of the Circle is " + areaOfCircle)
-alert("The circumference of the Circle is " + circumferenceOfCircle)
-console.log({ radius })
-console.log({ pi })
-console.log({ areaOfCircle })
-console.log({ circumferenceOfCircle })
+alert("The area of the Circle " + Math.round(areaOfCircle))
+alert("The circumference of the circle is " + Math.round(circumferenceOfCircle))
 
-var hours = prompt("Enter hours")
-var ratePerHour = prompt("Enter rate per hour")
-var pay = hours * ratePerHour
+//5. Calculate the slope, x - intercept and y - intercept of y = 2x - 2
+
+//6. Slope is m = (y2 - y1) / (x2 - x1).Find the slope between point(2, 2) and point(6, 10)
+
+//7. Compare the slope of above two questions.
+
+//8. Calculate the value of y(y = x2 + 6x + 9).Try to use different x values and figure out at what x value y is 0.
+
+//9. Write a script that prompt a user to enter hours and rate per hour.Calculate pay of the person ?
+
+// Enter hours: 40
+// Enter rate per hour: 28
+// Your weekly earning is 1120
+let hours = prompt("Enter hours")
+let ratePerHour = prompt("Enter rate per hour")
+let pay = hours * ratePerHour
 alert("Your weekly earning is " + pay)
-console.log(hours)
-console.log(ratePerHour)
-console.log(pay)
 
-console.log("Abdussomad".length)
-let nameLong = "long"
-console.log(nameLong)
-let firstNamee = 'Abdussomad'
-let lastNamee = 'Abdulqodir'
-console.log(`My firstname ${firstNamee}, has the same length with our family name, ${lastNamee}`)
-let myAge = 250
-let yourAge = 25
-let older = 250 - 25
-console.log(`I am ${older} years older than you.`)
 
-var yearOld = prompt("How old are you?")
-if (yearOld >= 18) {
-    alert("You are " + yearOld + ". You are old enough to drive")
-
+//10. If the length of your name is greater than 7 say, your name is long else say your name is short.
+let myName = "Abdussomad"
+if (myName.length > 7) {
+    console.log("Your name is long.")
 } else {
-    alert("You are" + yearOld + ". You will be allowed to drive after" + (+18 - yearOld) + "years.")
+    console.log("Your name is short.")
 }
 
-var yearsLived = prompt("Enter number of years you lived.")
-var secLived = new Date().getSeconds()
-alert("You lived" + secLived + "seconds.")
+//11. Compare your first name length and your family name length and you should get this output.
 
-var createYear = new Date().getFullYear()
-var createMonth = new Date().getMonth()
-var createDate = new Date().getDate()
-var createHour = new Date().getHours()
-var createMinute = new Date().getMinutes()
+// let firstName = 'Asabeneh'
+// let lastName = 'Yetayeh'
+// Your first name, Asabeneh is longer than your family name, Yetayeh
+
+let myFirstName = 'Abdussomad'
+let familyName = 'Pa-aranti'
+if (myFirstName.length > familyName.length) {
+    console.log(`Your first name, ${myFirstName} is longer than your family name, ${familyName}`)
+} else {
+    console.log(`Your family name, ${familyName} is longer than your first name, ${myFirstName}`)
+}
+
+
+//12. Declare two variables myAge and yourAge and assign them initial values and myAge and yourAge.
+
+// let myAge = 250
+// let yourAge = 25
+// I am 225 years older than you.
+let myAge = 250
+let yourAge = 25
+if (myAge > yourAge) {
+    console.log(`I am ${myAge - yourAge} years older than you.`)
+} else {
+    console.log(`You are ${yourAge - myAge} years older than me.`)
+}
+
+//13. Using prompt get the year the user was born and if the user is 18 or above allow the user to drive if not tell the user to wait a certain amount of years.
+
+// Enter birth year: 1995
+// You are 25. You are old enough to drive
+
+// Enter birth year: 2005
+// You are 15. You will be allowed to drive after 3 years.
+
+let userYear = prompt("Enter birth year:")
+let userAge = now.getFullYear() - userYear
+if (userAge >= 18) {
+    alert(`You are ${userAge}. You are old enough to drive`)
+} else {
+    alert(`You are ${userAge}. You will be allowed to drive after ${18 - userAge} years.`)
+}
+let agfh = "ekfdjl"
+//14. Write a script that prompt the user to enter number of years.Calculate the number of seconds a person can live.Assume some one lives just hundred years
+// Enter number of years you live: 100
+// You lived 3153600000 seconds.
+let enterYears = prompt("Enter number of years you live:")
+let secondsLived = enterYears * 31536000
+alert(`You lived ${secondsLived} seconds.`)
+
+//15. Create a human readable time format using the Date time object
+
+// YYYY - MM - DD HH: mm
+// DD - MM - YYYY HH: mm
+// DD / MM / YYYY HH: mm
+
+let createYear = new Date().getFullYear()
+let createMonth = new Date().getMonth()
+let createDate = new Date().getDate()
+let createHour = new Date().getHours()
+let createMinute = new Date().getMinutes()
 
 console.log(`${createYear}-${createMonth}-${createDate} ${createHour}:${createMinute}`)
 console.log(`${createDate}-${createMonth}-${createYear} ${createHour}:${createMinute}`)

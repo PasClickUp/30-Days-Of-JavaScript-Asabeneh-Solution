239f0324d09ed2e37d111a4c595c6e3758f6df5f
Abdussomad Pa-aranti <abdussomadabdulqodir@gmail.com>
2022-07-26 15:43:08 +0100

    Asabeneh

--- Day 12 Exercise/main.js
+++ Day 12 Exercise/main.js
@@ -1,10 +1,20 @@
 //LEVEL 1
 //1. Calculate the total annual income of the person from the following text. ‘He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.’
-let totalAnnualIncome = [4000, 10000, 5500].reduce((acc, cur) => acc + cur, 0)
+let textA = `He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.`
+let patternA = /\d+/g
+let toMatchA = textA.match(patternA)
+let totalAnnualIncome = toMatchA.reduce((acc, cur, i, arr) => acc +=
+    i === 0 || i === arr.length - 1 ? +cur * 12 : +cur, 0)
+console.log(totalAnnualIncome)
 //2. The position of some particles on the horizontal x - axis - 12, -4, -3 and - 1 in the negative direction, 0 at origin, 4 and 8 in the positive direction.Extract these numbers and find the distance between the two furthest particles.
-// points = ['-1', '2', '-4', '-3', '-1', '0', '4', '8']
+// points = ['-12', '-4', '-3', '-1', '0', '4', '8']
 // sortedPoints = [-4, -3, -1, -1, 0, 2, 4, 8]
 // distance = 12
+let textB = `The position of some particles on the horizontal x-axis -12, -4, -3 and -1 in the negative direction, 0 at origin, 4 and 8 in the positive direction.`
+let patternB = /-\d+|\d+/g
+let toMatchB = textB.match(patternB)
+let distance = toMatchB.reduce((a, b) => a += b > 0 ? +b : 0, 0)
+console.log(distance)
 //1. Write a pattern which identify if a string is a valid JavaScript variable
 
 // is_valid_variable('first_name') // True
@@ -15,31 +25,45 @@ let totalAnnualIncome = [4000, 10000, 5500].reduce((acc, cur) => acc + cur, 0)
 //1. Write a function called tenMostFrequentWords which get the ten most frequent word from a string ?
 
 let paragraph = `I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.`
-// console.log(tenMostFrequentWords(paragraph))
-[
-    { word: 'love', count: 6 },
-    { word: 'you', count: 5 },
-    { word: 'can', count: 3 },
-    { word: 'what', count: 2 },
-    { word: 'teaching', count: 2 },
-    { word: 'not', count: 2 },
-    { word: 'else', count: 2 },
-    { word: 'do', count: 2 },
-    { word: 'I', count: 2 },
-    { word: 'which', count: 1 },
-    { word: 'to', count: 1 },
-    { word: 'the', count: 1 },
-    { word: 'something', count: 1 },
-    { word: 'if', count: 1 },
-    { word: 'give', count: 1 },
-    { word: 'develop', count: 1 },
-    { word: 'capabilities', count: 1 },
-    { word: 'application', count: 1 },
-    { word: 'an', count: 1 },
-    { word: 'all', count: 1 },
-    { word: 'Python', count: 1 },
-    { word: 'If', count: 1 }]
-// console.log(tenMostFrequentWords(paragraph, 10))
+
+
+let tenMostFrequentWords = (arr, num) => {
+    let patternC = /\w+/g
+    let frequentWords = arr.match(patternC)
+    let frequentWordsNumbers = frequentWords.reduce((a, b) => {
+        a[b] = a[b] + 1 || 1
+
+        return a
+    }, {})
+    let toTestReduce = Object.entries(frequentWordsNumbers).sort((a, b) => b[1] - a[1]).map((val) => ({ word: val[0], count: val[1] })
+    ).slice(0, num)
+    return toTestReduce
+}
+console.log(tenMostFrequentWords(paragraph))
+// [
+//     { word: 'love', count: 6 },
+//     { word: 'you', count: 5 },
+//     { word: 'can', count: 3 },
+//     { word: 'what', count: 2 },
+//     { word: 'teaching', count: 2 },
+//     { word: 'not', count: 2 },
+//     { word: 'else', count: 2 },
+//     { word: 'do', count: 2 },
+//     { word: 'I', count: 2 },
+//     { word: 'which', count: 1 },
+//     { word: 'to', count: 1 },
+//     { word: 'the', count: 1 },
+//     { word: 'something', count: 1 },
+//     { word: 'if', count: 1 },
+//     { word: 'give', count: 1 },
+//     { word: 'develop', count: 1 },
+//     { word: 'capabilities', count: 1 },
+//     { word: 'application', count: 1 },
+//     { word: 'an', count: 1 },
+//     { word: 'all', count: 1 },
+//     { word: 'Python', count: 1 },
+//     { word: 'If', count: 1 }]
+console.log(tenMostFrequentWords(paragraph, 10))
 // [{ word: 'love', count: 6 },
 // { word: 'you', count: 5 },
 // { word: 'can', count: 3 },
@@ -54,11 +78,23 @@ let paragraph = `I love teaching. If you do not love teaching what else can you 
 //LEVEL 3
 //1. Writ a function which cleans text.Clean the following text.After cleaning, count three most frequent words in the string.
 let sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`
-// console.log(cleanText(sentence))
+let cleanText = (arr) => {
+    let result = arr.replace(/%|@|$|&|;|#|!|\$/g, "")
+    return result
+}
+console.log(cleanText(sentence))
 //  I am a teacher and I love teaching There is nothing as more rewarding as educating and empowering people I found teaching more interesting than any other jobs Does this motivate you to be a teacher
-//     ```
+//
 //2. Write a function which find the most frequent words. After cleaning, count three most frequent words in the string.
 
-// ```js
-// console.log(mostFrequentWords(cleanedText))
-[{ word: 'I', count: 3 }, { word: 'teaching', count: 2 }, { word: 'teacher', count: 2 }]
+let mostFrequentWords = (arr) => {
+    let patternE = /\w+/g
+    let frequentWordsB = arr.match(patternE).reduce((a, b) => {
+        a[b] = a[b] + 1 || 1
+        return a
+    }, {})
+    let threeFrequent = Object.entries(frequentWordsB).sort((a, b) => b[1] - a[1]).map((word) => ({ word: word[0], count: word[1] })).slice(0, 3)
+    return threeFrequent
+}
+console.log(mostFrequentWords(sentence))
+
--- Day 15 Exercise/main.js
+++ Day 15 Exercise/main.js
@@ -1,29 +1,94 @@
-class practising {
-    constructor(firstName, wife, number, job) {
-        console.log(this)
-        this.firstName = firstName
-        this.wife = wife
-        this.number = number
-        this.job = job
-        this.skills = []
+//LEVEL 1
+//1. Create an Animal class. The class will have name, age, color, legs properties and create different methods
+class Animal {
+    constructor(name, age, color, legs) {
+        this.name = name
+        this.age = age
+        this.color = color
+        this.legs = legs
     }
-
-    get forNameJob() {
-        let nameJob = `${this.firstName}'s job is ${this.job} `
-        return nameJob
-    }
-    get getSkills() {
-        return this.skills
-    }
-    set setSkill(skill) {
-        this.skills.push(skill)
+    get nameLeg() {
+        return `${this.name} has ${this.legs} legs`
     }
 }
+//2. Create a Dog and Cat child class from the Animal Class.
+let Dog = new Animal("Kalbun", 3, "brown", 4)
+let Cat = new Animal("Hurairatun", 2, "white", 4)
+//LEVEL 2
+//1. Override the method you create in Animal class
+class child extends Animal {
+    constructor(name, age, color, legs, hands, gender) {
+        this.gender = gender
+        this.hands = hands
+    }
+    get nameLeg() {
+        return `${this.name} has ${this.legs} legs`
+    }
+}
+let Monkey = new Animal("Qirdun", 5, "brown", 2, 2, "male")
+//LEVEL 3
+//1. Let's try to develop a program which calculate measure of central tendency of a sample(mean, median, mode) and measure of variability(range, variance, standard deviation). In addition to those measures find the min, max, count, percentile, and frequency distribution of the sample. You can create a class called Statistics and create all the functions which do statistical calculations as method for the Statistics class. Check the output below.
+// ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
 
-let mySelf = new practising("Abdussomad", "Zainab", 1, "Software Developer")
-let myFriend = new practising("Baasit", "Maryam", "2", "Business")
-my
-myFriend.setSkill = "freelancing"
-// mySelf.
-console.log(mySelf, myFriend.forNameJob, myFriend.skills)
+// console.log('Count:', statistics.count()) // 25
+// console.log('Sum: ', statistics.sum()) // 744
+// console.log('Min: ', statistics.min()) // 24
+// console.log('Max: ', statistics.max()) // 38
+// console.log('Range: ', statistics.range() // 14
+// console.log('Mean: ', statistics.mean()) // 30
+// console.log('Median: ', statistics.median()) // 29
+// console.log('Mode: ', statistics.mode()) // {'mode': 26, 'count': 5}
+// console.log('Variance: ', statistics.var()) // 17.5
+// console.log('Standard Deviation: ', statistics.std()) // 4.2
+// console.log('Variance: ', statistics.var()) // 17.5
+// console.log('Frequency Distribution: ', statistics.freqDist()) // [(20.0, 26), (16.0, 27), (12.0, 32), (8.0, 37), (8.0, 34), (8.0, 33), (8.0, 31), (8.0, 24), (4.0, 38), (4.0, 29), (4.0, 25)]
+// // you output should look like this
+// console.log(statistics.describe())
+// Count: 25
+// Sum: 744
+// Min: 24
+// Max: 38
+// Range: 14
+// Mean: 30
+// Median: 29
+// Mode: (26, 5)
+// Variance: 17.5
+// Standard Deviation: 4.2
+// Frequency Distribution: [(20.0, 26), (16.0, 27), (12.0, 32), (8.0, 37), (8.0, 34), (8.0, 33), (8.0, 31), (8.0, 24), (4.0, 38), (4.0, 29), (4.0, 25)]
+//2. Create a class called PersonAccount.It has firstname, lastname, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo, addIncome, addExpense and accountBalance methods.Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
 
+class PersonAccount {
+    constructor(firstname, lastname) {
+        this.firstname = firstname
+        this.lastname = lastname
+        this.incomes = []
+        this.expenses = []
+    }
+    addIncome(income) {
+        return this.incomes.push(income)
+    }
+    addExpense(expense) {
+        return this.expenses.push(expense)
+    }
+    get totalIncome() {
+        return this.incomes.reduce((a, b) => a + b, 0)
+    }
+    get totalExpense() {
+        return this.expenses.reduce((a, b) => a + b, 0)
+    }
+    get accountBalance() {
+        return `${this.totalIncome - this.totalExpense}`
+    }
+    get accountInfo() {
+        let fullName = `${this.firstname} ${this.lastname}`
+        let info = `${fullName} average monthly income is ${this.totalIncome}, His current account balance is ${this.accountBalance}`
+        return info
+    }
+}
+let someone = new PersonAccount("Fulaan", "Fulaan")
+someone.addIncome(1000)
+someone.addIncome(4000)
+someone.addIncome(15000)
+someone.addExpense(1000)
+someone.addExpense(8000)
+console.log(someone)

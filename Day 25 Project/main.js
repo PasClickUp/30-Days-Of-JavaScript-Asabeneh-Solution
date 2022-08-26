bd037cfa00d2d807307b8a28fa1a0de935ec2f50
Abdussomad Pa-aranti <abdussomadabdulqodir@gmail.com>
2022-08-26 15:36:31 +0100

    Asabeneh

--- Day 25 Project/main.js
+++ Day 25 Project/main.js
@@ -15,22 +15,49 @@ paragraph.forEach(p => {
     }
 })
 
-// let tenMostSpokenLanguages = Object.entries(countriesData.flatMap(({ languages }) =>
-//     languages
-// ).reduce((acc, cur) => {
-//     acc[cur] = acc[cur] + 1 || 1
-//     return acc
-// }, {})).sort((a, b) => b[1] - a[1]).slice(0, 10)
+const populationFunction = () => {
+    let worldPopulation = countriesData.reduce((acc, { population }) => { return acc + population }, 0)
+    let sortingPopulation = countriesData.sort((a, b) => b.population - a.population).slice(0, 10)
 
-// let worldPopulation = countriesData.map(({ population }) => population).reduce((acc, cur) => {
-//     return acc + cur
-// }, 0)
+    graphWrapper.innerHTML = `
+    <span>World</span>
+    <span class = 'range'; style = 'width :100%'></span>
+    <span>${worldPopulation.toLocaleString()}
+    `
 
-// let worldDetails = {
-//     name: 'world', population: worldPopulation
-// }
-// countriesData.push(worldDetails)
-// console.log(
-//     countriesData
-// )
+    sortingPopulation.forEach(country => {
 
+        graphWrapper.innerHTML += `<span>${country.name}</span>
+
+        <span class='range' style = 'width:${Number(country.population * 100 / worldPopulation)}%'></span>
+        
+        <span>${(country.population).toLocaleString()}</span>
+        `
+
+    })
+    graphTitle.textContent = '10 Most populated countries in the world'
+}
+
+const languageFunction = () => {
+    graphWrapper.innerHTML = ''
+    const mostLanguagesSort = Object.entries(countriesData.flatMap(({ languages }) => languages).reduce((acc, cur) => {
+        acc[cur] ? acc[cur] += 1 : acc[cur] = 1;
+        return acc
+    }, {})).sort((a, b) => b[1] - a[1]).slice(0, 10)
+
+    mostLanguagesSort.forEach(language => {
+        graphWrapper.innerHTML += `<span>${language[0]}</span><span class = 'range' style='width:${Number((language[1] * 100) / 100)}%; '></span><span>${language[1]}</span>`
+    })
+    graphTitle.textContent = '10 Most Spoken languages in the world'
+}
+
+button.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        let onPress = e.currentTarget.classList
+        onPress.contains('population') ? populationFunction() :
+            onPress.contains('languages') ? languageFunction() :
+                false
+    })
+})
+
+languageFunction()
